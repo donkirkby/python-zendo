821fca97a24b305c0f4098d49baaf6cc12708680
Don Kirkby <donkirkby@gmail.com>
2019-04-29 21:19:37 -0700

    Add Ace editor, and a button to hide or show it.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import AceEditor from 'react-ace';
 import './App.css';
 
+import 'brace/mode/python';
+import 'brace/theme/github';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rule: "",
+      is_rule_visible: true,
+      visibility_button_name: "Hide Rule",
+      inputs: []  // [{ text: "...", follows_rule: true}]
+    };
+  }
+
+  handleRuleChange = rule_text => {
+    this.setState({
+      rule: rule_text
+    });
+  };
+
+  handleVisibilityChange = () => {
+    if (this.state.is_rule_visible) {
+      this.setState({
+        is_rule_visible: false,
+        visibility_button_name: "Show Rule"
+      });
+    } else {
+      this.setState({
+        is_rule_visible: true,
+        visibility_button_name: "Hide Rule"
+      });
+    }
+
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button
+            type="button"
+            onClick={this.handleVisibilityChange}
+            className="small"
+        >{this.state.visibility_button_name}</button>
+        {this.state.is_rule_visible && <AceEditor
+            value={this.state.rule}
+            onChange={this.handleRuleChange}
+            mode="python"
+            theme="github"
+            fontSize={18}
+            showPrintMargin={true}
+            showGutter={true}
+            highlightActiveLine={true}
+            editorProps={{
+                $blockScrolling: Infinity,
+                visibility: this.state.rule_visibility
+            }}
+            setOptions={{
+                showLineNumbers: true,
+                tabSize: 4,
+            }}/>}
       </div>
     );
   }
