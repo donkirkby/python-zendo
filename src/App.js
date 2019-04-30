37b3e8a1f603adeb30440fab4b8ad8beb116e3da
Don Kirkby <donkirkby@gmail.com>
2019-04-29 21:48:50 -0700

    Add list of input fields as part of #2.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       rule: "",
       is_rule_visible: true,
       visibility_button_name: "Hide Rule",
+      new_input: "",
       inputs: []  // [{ text: "...", follows_rule: true}]
     };
   }
@@ -19,6 +20,12 @@ class App extends Component {
   handleRuleChange = rule_text => {
     this.setState({
       rule: rule_text
+    });
+  };
+
+  handleInputChange = evt => {
+    this.setState({
+      new_input: evt.target.value
     });
   };
 
@@ -35,6 +42,19 @@ class App extends Component {
       });
     }
 
+  };
+
+  handleNewInput = () => {
+    if (this.state.new_input === "") {
+        return;
+    }
+    this.setState({
+        new_input: "",
+        inputs: this.state.inputs.concat([{
+            text: this.state.new_input,
+            follows_rule: false
+        }])
+    });
   };
 
   render() {
@@ -62,6 +82,15 @@ class App extends Component {
                 showLineNumbers: true,
                 tabSize: 4,
             }}/>}
+          {this.state.inputs.map((entry, entry_index) => (
+              <pre key={entry_index}>{entry.text}</pre>
+          ))}
+          <input
+              type="text"
+              placeholder="Type input here."
+              value={this.state.new_input}
+              onChange={this.handleInputChange}/>
+          <button type="button" onClick={this.handleNewInput} className="small">Tell</button>
       </div>
     );
   }
