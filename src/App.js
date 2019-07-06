1d960b952f500bec3a5fcbd1e213f944bab966f0
Don Kirkby <donkirkby@gmail.com>
2019-07-06 10:13:13 -0700

    Hide and show the guess editor. Highlight diffs. Part of #5.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import './App.css';
 import rule_runner from './rule_runner.py';
 import thumb_up from './thumb-up-3x.png';
 import thumb_down from './thumb-down-3x.png';
+import thumb_up_red from './thumb-up-red-3x.png';
+import thumb_down_red from './thumb-down-red-3x.png';
 import blank from './blank.png';
 
 import 'brace/mode/python';
 import 'brace/theme/github';
+
+const RULE_EXAMPLE = `\
+input_text = input()
+is_rule_followed = True  # Replace this.
+print(is_rule_followed)
+`;
 
 class App extends Component {
     constructor() {
@@ -16,11 +24,10 @@ class App extends Component {
             rule_text = canSave ? window.localStorage.zendoRule : '',
             rule_guess = canSave ? window.localStorage.zendoRuleGuess : '';
         if (rule_text === '' || rule_text === undefined) {
-            rule_text = `\
-input_text = input().strip()
-is_rule_followed = len(input_text) <= 4
-print(is_rule_followed)
-`;
+            rule_text = RULE_EXAMPLE;
+        }
+        if (rule_guess === '' || rule_guess === undefined) {
+            rule_guess = RULE_EXAMPLE;
         }
         this.state = {
             rule: rule_text,
@@ -28,6 +35,8 @@ print(is_rule_followed)
             stdout: "",
             is_rule_visible: true,
             visibility_button_name: "Hide Rule",
+            is_guess_visible: false,
+            guess_button_name: "Show Guess",
             new_input: "",
             inputs: [],  // [{ text: "...", follows_rule: true}]
             can_save: canSave,
@@ -67,28 +76,41 @@ print(is_rule_followed)
         this.updateRules(this.state.rule, rule_guess);
     };
 
-    updateRules = (rule_text, rule_guess) => {
+    updateRules = (rule_text, rule_guess, is_guess_visible) => {
+        if (is_guess_visible === undefined) {
+            is_guess_visible = this.state.is_guess_visible;
+        }
+        let updateInput = this.updateInput;
         let new_inputs = this.state.inputs.map(function(old_input) {
-            let new_input = {text: old_input.text};
-            if (rule_text && rule_text.length > 0) {
-                let rule_results = window.check_rule(rule_text, old_input.text);
-                new_input.follows_rule = rule_results[0];
-            }
-            if (rule_guess && rule_guess.length > 0) {
-                let rule_results = window.check_rule(rule_guess, old_input.text);
-                new_input.follows_rule_guess = rule_results[0];
-            }
-            return new_input;
+            return updateInput(old_input, rule_text, is_guess_visible && rule_guess);
         });
         this.setState({
             rule: rule_text,
             rule_guess: rule_guess,
+            is_guess_visible: is_guess_visible,
             inputs: new_inputs
         });
         if (this.state.can_save && !this.state.will_save) {
             window.setTimeout(this.saveBackup, 1000);
             this.setState({will_save: true});
         }
+    };
+
+    updateInput = (old_input, rule_text, rule_guess) => {
+        let new_input = {text: old_input.text};
+        if (rule_text && rule_text.length > 0) {
+            let rule_results = window.check_rule(rule_text, old_input.text);
+            new_input.follows_rule = rule_results[0];
+            new_input.rule_output = rule_results[1];
+        }
+        if (rule_guess && rule_guess.length > 0) {
+            let rule_results = window.check_rule(rule_guess, old_input.text);
+            new_input.follows_rule_guess = rule_results[0];
+            new_input.guess_output = rule_results[1];
+            new_input.is_mismatch = (
+                new_input.follows_rule !== new_input.follows_rule_guess);
+        }
+        return new_input;
     };
 
     saveBackup = () => {
@@ -118,6 +140,15 @@ print(is_rule_followed)
 
     };
 
+    handleGuessVisibilityChange = () => {
+        let is_guess_visible = ! this.state.is_guess_visible;
+        this.setState({
+            is_guess_visible: is_guess_visible,
+            guess_button_name: is_guess_visible ? "Hide Guess" : "Show Guess"
+        });
+        this.updateRules(this.state.rule, this.state.rule_guess, is_guess_visible);
+    };
+
     handleInputKeyPress = event => {
         if (event.key === 'Enter') {
             this.handleNewInput();
@@ -128,26 +159,14 @@ print(is_rule_followed)
         if (this.state.new_input === "") {
             return;
         }
-        let rule_results = window.check_rule(
+        let new_input_entry = this.updateInput(
+            {text: this.state.new_input},
             this.state.rule,
-            this.state.new_input);
-        let is_rule_followed = rule_results[0],
-            output_text = rule_results[1];
-        let is_rule_guess_followed = undefined;
-        if (this.state.rule_guess && this.state.rule_guess.length > 0) {
-            rule_results = window.check_rule(
-                this.state.rule_guess,
-                this.state.new_input);
-            is_rule_guess_followed = rule_results[0];
-        }
+            this.state.is_guess_visible && this.state.rule_guess);
         this.setState({
             new_input: "",
-            stdout: output_text,
-            inputs: this.state.inputs.concat([{
-                text: this.state.new_input,
-                follows_rule: is_rule_followed,
-                follows_rule_guess: is_rule_guess_followed
-            }])
+            stdout: new_input_entry.rule_output,
+            inputs: this.state.inputs.concat([new_input_entry])
         });
     };
 
@@ -170,8 +189,7 @@ print(is_rule_followed)
                     showGutter={false}
                     highlightActiveLine={true}
                     editorProps={{
-                        $blockScrolling: Infinity,
-                        visibility: this.state.rule_visibility
+                        $blockScrolling: Infinity
                     }}
                     setOptions={{
                         showLineNumbers: false,
@@ -180,47 +198,10 @@ print(is_rule_followed)
                 <p>Output:</p>
                 {this.state.is_rule_visible && <AceEditor
                     value={this.state.stdout}
+                    mode="text"
                     theme="github"
                     width="100%"
                     height="10em"
-                    fontSize={18}
-                    showPrintMargin={true}
-                    showGutter={false}
-                    highlightActiveLine={true}
-                    editorProps={{
-                        $blockScrolling: Infinity,
-                        visibility: this.state.rule_visibility
-                    }}
-                    setOptions={{
-                        showLineNumbers: false,
-                        tabSize: 4,
-                    }}/>}
-                <ul>
-                    {this.state.inputs.map((entry, entry_index) => (
-                        <li key={"item" + entry_index}>
-                            <img alt={entry.follows_rule} src={(entry.follows_rule && thumb_up) || thumb_down}/>
-                            <pre key={"item_text" + entry_index} style={{display: "inline"}}>{entry.text}</pre>
-                            <img alt={entry.follows_rule_guess}
-                                 src={(entry.follows_rule_guess && thumb_up) ||
-                                 (entry.follows_rule_guess === undefined && blank) ||
-                                 thumb_down}/>
-                        </li>
-                    ))}
-                </ul>
-                <input
-                    type="text"
-                    placeholder="Type input here."
-                    value={this.state.new_input}
-                    onChange={this.handleInputChange}
-                    onKeyPress={this.handleInputKeyPress}/>
-                <button type="button" onClick={this.handleNewInput} className="small">Tell</button>
-                <p>Guess the rule:</p>
-                <AceEditor
-                    value={this.state.rule_guess}
-                    onChange={this.handleRuleGuessChange}
-                    mode="python"
-                    theme="github"
-                    width="100%"
                     fontSize={18}
                     showPrintMargin={true}
                     showGutter={false}
@@ -231,7 +212,56 @@ print(is_rule_followed)
                     setOptions={{
                         showLineNumbers: false,
                         tabSize: 4,
-                    }}/>
+                    }}/>}
+                <ul>
+
+                    {this.state.inputs.map((entry, entry_index) => (
+                        <li key={"item" + entry_index}>
+                            <img alt={entry.follows_rule} src={(entry.follows_rule && thumb_up) || thumb_down}/>
+                            <pre key={"item_text" + entry_index} style={{display: "inline"}}>{entry.text}</pre>
+                            <img alt={entry.follows_rule_guess}
+                                 src={(entry.follows_rule_guess === undefined && blank) ||
+                                 (entry.is_mismatch &&
+                                     ((entry.follows_rule_guess && thumb_up_red) || thumb_down_red)) ||
+                                     ((entry.follows_rule_guess && thumb_up) || thumb_down)
+                                 }/>
+                        </li>
+                    ))}
+                </ul>
+                <input
+                    type="text"
+                    placeholder="Type input here."
+                    value={this.state.new_input}
+                    onChange={this.handleInputChange}
+                    onKeyPress={this.handleInputKeyPress}/>
+                <button type="button" onClick={this.handleNewInput} className="small">Tell</button>
+                <p>
+                <button
+                    type="button"
+                    onClick={this.handleGuessVisibilityChange}
+                    className="small"
+                >{this.state.guess_button_name}</button>
+                </p>
+                {this.state.is_guess_visible && <div>
+                    <p>Guess the rule:</p>
+                    <AceEditor
+                        value={this.state.rule_guess}
+                        onChange={this.handleRuleGuessChange}
+                        mode="python"
+                        theme="github"
+                        width="100%"
+                        fontSize={18}
+                        showPrintMargin={true}
+                        showGutter={false}
+                        highlightActiveLine={true}
+                        editorProps={{
+                            $blockScrolling: Infinity
+                        }}
+                        setOptions={{
+                            showLineNumbers: false,
+                            tabSize: 4,
+                        }}/>
+                </div>}
             </div>
         );
     }
