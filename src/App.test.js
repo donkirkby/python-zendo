27e7720ffec900f9c143cb171b137089571a36b9
Don Kirkby <donkirkby@gmail.com>
2025-03-03 19:40:57 -0800

    Upgrade react-dom, and switch to createRoot().

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div'),
+        root = createRoot(div),
         mockDataSource = {};
-  ReactDOM.render(<App dataSource={mockDataSource}/>, div);
+  root.render(<App dataSource={mockDataSource}/>);
 });
